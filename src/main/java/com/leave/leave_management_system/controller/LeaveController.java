074b8e0f50131d4bd14e9edae1afc942ed7acf1e
Rishita Jain <rishitawork11@gmail.com>
2026-07-16 14:38:18 +0530

    change url for request mapping

--- src/main/java/com/leave/leave_management_system/controller/LeaveController.java
+++ src/main/java/com/leave/leave_management_system/controller/LeaveController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/leave")
+@RequestMapping("/api/leave")
 @CrossOrigin("*")
 public class LeaveController {
 
